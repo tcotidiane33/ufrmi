3da180822bac974d8249bf22975fed38115e33ed
eureka <tcotidiane33@gmail.com>
2022-06-08 19:04:23 +0000

    update project

---
+++ README.md
@@ -0,0 +1,1 @@
+my personnal token acess `{ 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 }`
---
+++ javascript/java.js
@@ -0,0 +1,145 @@
+//navbar 
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+function myFunction_parc(){
+  document.getElementById("myDropdown_parc").classList.toggle("show");
+  
+}
+function myFunction_lab(){
+    document.getElementById("myDropdown_lab").classList.toggle("show");
+}
+function myFunction_space(){
+  document.getElementById("myDropdown_space").classList.toggle("show");
+  
+}
+
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+// PROMOTION HEADER
+const target = document.getElementById("target");
+let array = [
+  "LMD",
+  "LICENCE ",
+  "MASTER",
+  "DOCTORAT",
+  "ACTUARIAT",
+  "MIAGE",
+  "MATHEMATIQUES",
+  "MECANIQUE",
+  "INFORMATIQUE",
+  "BDGL",
+  "PROBABILITÉ",
+];
+let wordIndex = 0;
+let letterIndex = 0;
+
+const createLetter = () => {
+  const letter = document.createElement("span");
+  target.appendChild(letter);
+
+  letter.classList.add("letter");
+  letter.style.opacity = "0";
+  letter.style.animation = "anim 5s ease forwards";
+  letter.textContent = array[wordIndex][letterIndex];
+
+  setTimeout(() => {
+    letter.remove();
+  }, 2000);
+};
+
+const loop = () => {
+  setTimeout(() => {
+    if (wordIndex >= array.length) {
+      wordIndex = 0;
+      letterIndex = 0;
+      loop();
+    } else if (letterIndex < array[wordIndex].length) {
+      createLetter();
+      letterIndex++;
+      loop();
+    } else {
+      letterIndex = 0;
+      wordIndex++;
+      setTimeout(() => {
+        loop();
+      }, 2000);
+    }
+  }, 80);
+};
+loop();
+// END PROMOTION
+// good coding
+
+
+
+// nav-right
+
+
+"use strict"; 
+
+const body = document.body;
+const bgColorsBody = ["#ffb457", "#ff96bd", "#9999fb", "#ffe797", "#cffff1"];
+const menu = body.querySelector(".menu");
+const menuItems = menu.querySelectorAll(".menu__item");
+const menuBorder = menu.querySelector(".menu__border");
+let activeItem = menu.querySelector(".active");
+
+function clickItem(item, index) {
+
+    menu.style.removeProperty("--timeOut");
+    
+    if (activeItem == item) return;
+    
+    if (activeItem) {
+        activeItem.classList.remove("active");
+    }
+
+    
+    item.classList.add("active");
+    body.style.backgroundColor = bgColorsBody[index];
+    activeItem = item;
+    offsetMenuBorder(activeItem, menuBorder);
+    
+    
+}
+
+function offsetMenuBorder(element, menuBorder) {
+
+    const offsetActiveItem = element.getBoundingClientRect();
+    const left = Math.floor(offsetActiveItem.left - menu.offsetLeft - (menuBorder.offsetWidth  - offsetActiveItem.width) / 2) +  "px";
+    menuBorder.style.transform = `translate3d(${left}, 0 , 0)`;
+
+}
+
+offsetMenuBorder(activeItem, menuBorder);
+
+menuItems.forEach((item, index) => {
+
+    item.addEventListener("click", () => clickItem(item, index));
+    
+})
+
+window.addEventListener("resize", () => {
+    offsetMenuBorder(activeItem, menuBorder);
+    menu.style.setProperty("--timeOut", "none");
+});
+
+
+
+// GALERIE
+/
